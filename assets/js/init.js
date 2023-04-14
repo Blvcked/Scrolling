f8cf0506c7a689b47ea2bd571a6f54d2b4137b31
Miles AB <hey@itsmil.es>
2023-04-14 21:56:11 +0200

    Prettier

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,21 +1,21 @@
 function documentReady() {
-    jsToCss();
+	jsToCss();
 }
 document.addEventListener(`DOMContentLoaded`, documentReady, false);
 
 function widnowLoad() {
-    isNavbarSticked();
-    isThresholdSurpassed();
-    isFooterInview();
-    getScrollProgress();
+	isNavbarSticked();
+	isThresholdSurpassed();
+	isFooterInview();
+	getScrollProgress();
 }
 window.addEventListener(`load`, widnowLoad, false);
 
 function widnowScroll() {
-    isNavbarSticked();
-    isThresholdSurpassed();
-    isFooterInview();
-    getScrollDirection();
-    getScrollProgress();
+	isNavbarSticked();
+	isThresholdSurpassed();
+	isFooterInview();
+	getScrollDirection();
+	getScrollProgress();
 }
 window.addEventListener(`scroll`, widnowScroll, { passive: true });
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
 function jsToCss() {
-    if (document.querySelector(`#siteHeader`)) {
-        const target = document.querySelector(`#siteHeader`);
-        const targetHeight = target.getBoundingClientRect().height;
-        document
-            .querySelector(`:root`)
-            .style.setProperty(`--siteHeader-height`, `${targetHeight}px`);
-    }
-    if (document.querySelector(`#siteTopbar`)) {
-        const target = document.querySelector(`#siteTopbar`);
-        const targetHeight = target.getBoundingClientRect().height;
-        document
-            .querySelector(`:root`)
-            .style.setProperty(`--siteTopbar-height`, `${targetHeight}px`);
-    }
+	if (document.querySelector(`#siteHeader`)) {
+		const target = document.querySelector(`#siteHeader`);
+		const targetHeight = target.getBoundingClientRect().height;
+		document
+			.querySelector(`:root`)
+			.style.setProperty(`--siteHeader-height`, `${targetHeight}px`);
+	}
+	if (document.querySelector(`#siteTopbar`)) {
+		const target = document.querySelector(`#siteTopbar`);
+		const targetHeight = target.getBoundingClientRect().height;
+		document
+			.querySelector(`:root`)
+			.style.setProperty(`--siteTopbar-height`, `${targetHeight}px`);
+	}
 }
